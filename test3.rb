92a12f6d34a2459ff7f8465496b60eb2d1f6e5f8
Andy Wortman <andy.wortman@originate.com>
2014-08-27 14:28:55 -0700

    Set up test harness to run against a grammar and file

--- test3.rb
+++ test3.rb
@@ -1,9 +1,14 @@
 #! /usr/bin/env ruby
 
 require './poggler_parser'
+require './parser_parts/matcher'
 
 metagrammar = ARGV[0] || "formats/test1"
 
 file = File.open(metagrammar, "r")
 contents = file.read
-PogglerParser.parse!(contents)
+foo = PogglerParser.parse!(contents)
+
+data = File.open("file", "r").read
+puts data.bytes[2].to_s(16)
+foo.match(Matcher.new data)
