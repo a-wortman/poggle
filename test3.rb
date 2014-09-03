89f5480b17102cb37b46481eabbc5de540733611
Andy Wortman <andy.wortman@originate.com>
2014-09-02 19:31:26 -0700

    Allow arbitrary files + test helper script

--- test3.rb
+++ test3.rb
@@ -4,10 +4,11 @@ require './poggler_parser'
 require './parser_parts/matcher'
 
 metagrammar = ARGV[0] || "formats/test1"
+dataFile = ARGV[1] || "file"
 
-file = File.open(metagrammar, "r")
-contents = file.read
-foo = PogglerParser.parse!(contents)
+grammarFile = File.open(metagrammar, "r")
+contents = grammarFile.read
+parser = PogglerParser.parse!(contents)
 
-data = File.open("file", "r").read
-foo.match(Matcher.new data)
+data = File.open(dataFile, "r").read
+parser.match(Matcher.new data)
