feace055580e49e9678c79235b828f2b38d69615
Andy Wortman <andy.wortman@originate.com>
2015-02-05 00:56:14 -0800

    placeholder scopifier unit spec

---
+++ spec/scope/scopifier_spec.rb
@@ -0,0 +1,20 @@
+
+describe "Scopifier" do
+
+  context "#enscopen" do
+
+    it "enscopens specified instance variables" do
+
+      expect(1).to equal(1)
+
+    end
+
+    it "adds an @scope to the enscopened object" do
+
+      expect(2).to equal(2)
+
+    end
+
+  end
+
+end
