613e14f791e9a96c310c7317611c7778168548fb
a-wortman <ixineeringeverywhere@gmail.com>
2015-02-08 02:48:28 -0800

    initial changes to parse java

---
+++ parse.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+
+require './poggle_parser'
+require './parser_parts/matcher'
+
+metagrammar = ARGV[0] or raise "Provide the name of a grammar in 'formats/'"
+dataFile = ARGV[1] or raise "Provide the relative path of a file from here to parse"
+
+grammarContents = File.open("formats/#{metagrammar}", "r").read
+parser = PoggleParser.parse!(grammarContents)
+
+data = File.open(dataFile, "r").read
+parser.match(Matcher.new data)
+
