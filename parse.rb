4dc0bbe7f01e5298f9bb93b981aa30f67d4358cc
a-wortman <ixineeringeverywhere@gmail.com>
2015-02-11 00:56:13 -0800

    make parse.rb more useful

--- parse.rb
+++ parse.rb
@@ -10,5 +10,9 @@ grammarContents = File.open("formats/#{metagrammar}", "r").read
 parser = PoggleParser.parse!(grammarContents)
 
 data = File.open(dataFile, "r").read
-parser.match(Matcher.new data)
-
+matched = parser.match(Matcher.new data)
+if matched
+  puts "[+] Matched!"
+else
+  puts "[-] No match."
+end
